4dc2ebefe13db4e9bd0f682a74d0df78250500c3
ColeGebhard <113935979+ColeGebhard@users.noreply.github.com>
2023-02-16 16:54:48 -0700

    Delete requests.js

--- src/components/requests/requests.js
+++
@@ -1,9 +0,0 @@
-export async function deleteActivities (token, postid) {
-    try {
-      const response = await fetch(`${BASE_URL}${cohortName}/posts/${postid}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
