feb59a5b02948b4cad6e578349af6a53bdd52945
safirtrew <safirtrew@gmail.com>
2023-02-16 19:18:26 -0500

    get Activities added API

--- src/components/api/requests.js
+++ src/components/api/requests.js
@@ -1,76 +1,79 @@
-
-
-
 const URL = "https://fitnesstrac-kr.herokuapp.com/api/";
-export let token = localStorage.getItem('token')
+export let token = localStorage.getItem("token");
 
-console.log(token)
-
+console.log(token);
 
 export const register = async (username, password) => {
-    try {
-        const resp = await fetch(`${URL}users/register`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username,
-                password
-            })
-        })
-        console.log(resp)
+  try {
+    const resp = await fetch(`${URL}users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+    console.log(resp);
 
-        const data =  resp.json()
-        console.log(data)
-        return data;
-
-    } catch (error) {
-        console.error(error)
-    }
-
-}
+    const data = resp.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const login = async (username, password) => {
+  try {
+    const resp = await fetch(`${URL}users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+    const data = await resp.json();
+    console.log(data.token);
+    localStorage.setItem("token", data.token);
 
-    try {
-        const resp = await fetch(`${URL}users/login`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username,
-                password
-            })
-        })
-        const data = await resp.json()
-        console.log(data.token)
-        localStorage.setItem('token',data.token)
-        
-        return data
-    } catch (error) {
-        console.error(error)
-    }
-}
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const isUser = async (token) => {
+  try {
+    const resp = await fetch(`${URL}users/me`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-    try {
-
-        const resp = await fetch(`${URL}users/me`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-        });
-
-        const data = await resp.json();
-        if(data.username){
-        return data
-        }
-        return false
-    } catch (error) {
-        console.error(error)
+    const data = await resp.json();
+    if (data.username) {
+      return data;
     }
-}
+    return false;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const Activities = async () => {
+  try {
+    const resp = await fetch (`${URL}users/activities`, {
+        headers: {
+            'Content-type' : 'application/json',
+        },
+    }).then (response => response.json())
+    .then (result => {
+        console.log(result);
+    })
+    .catch (console.error);
