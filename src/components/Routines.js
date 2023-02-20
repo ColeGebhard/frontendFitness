47bbc8e6b9df4d5de0d2394478b7e4cba7dc816f
safirtrew <safirtrew@gmail.com>
2023-02-19 20:22:20 -0500

    routines & activities edited

--- src/components/Routines.js
+++ src/components/Routines.js
@@ -1,59 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { getRoutines } from "./api/requests";
+import { Link } from "react-router-dom";
 
-const Routines = ({
-    routines,
-    setRoutines,
-    token,
-    currentUser
-}) => {
+const Routines = ({ routines, setRoutines, token, currentUser }) => {
+  useEffect(() => {
+    getRoutines()
+      .then((routines) => {
+        setRoutines(routines);
+      })
+      .catch((e) => {
+        console.error("Failed to get routines");
+      });
+  }, [setRoutines]);
 
-    useEffect(() => {
-        getRoutines()
-        .then((routines) => {
-            setRoutines(routines);
-        })
-        .catch((e) => {
-            console.error('Failed to get routines')
-        });
-    }, [ setRoutines ])
+  console.log(routines);
 
-    console.log(routines)
-
-    return (
-        <div>
-        {
-            routines.map((routine) => {
-                return (
-                    <div className="routineCard">
-                    <h3>Routines</h3>
-                    <p>Name: { routine.name }</p>
-                    <p>goal: { routine.goal }</p>
-                    <p>User: { routine.creatorName }</p>
-                    <ul > Activities
-                        <li  className="routineActivityCard">{
-                            routine.activities.map((activity) => {
-                                return (
-                                    <div>
-                                        <ul>
-                                        <h5>{activity.name}</h5>
-                                        <p>Info: {activity.description}</p>
-                                        <p>Count: {activity.count}</p>
-                                        <p>Duration: {activity.duration}</p>
-                                        </ul>
-                                    </div>
-                                )
-                            }
-                            )
-                            }</li>
-                    </ul>
+  return (
+    <div className="activityCardContainer">
+      <h1> Routines</h1>
+      {routines.map((routine) => {
+        return (
+          <div className="routineCard">
+            <h3>Routines</h3>
+            <p>Name: {routine.name}</p>
+            <p>goal: {routine.goal}</p>
+            <p>User: {routine.creatorName}</p>
+            <ul>
+              {" "}
+              Activities
+              <li className="routineActivityCards">
+                {routine.activities.map((activity) => {
+                  return (
+                    <div className="routineActivityCard">
+                      <ul>
+                        <h5>{activity.name}</h5>
+                        <p>Info: {activity.description}</p>
+                        <p>Count: {activity.count}</p>
+                        <p>Duration: {activity.duration}</p>
+                      </ul>
                     </div>
-                )
-            })
-        }
-        </div>
-    )
-
-}
+                  );
+                })}
+              </li>
+            </ul>
+            <Link id="startRoutine" to={`/routines/${routine.id}`}>
+              Start Routine
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Routines;
